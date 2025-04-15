91af209c7e10d7e73796f9bf0fdee64595a3f2a3
Aditya Kothekar <122866305+AdityaK85@users.noreply.github.com>
2025-04-16 01:39:33 +0530

    tod

--- school/settings.py
+++ school/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'school.urls'
@@ -140,7 +141,7 @@ EMAIL_HOST_PASSWORD = 'saen udtq ompp pvqx'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
